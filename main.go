9df16ccbee18c0b779086549c8aac2f353399113
kingsuk-panda <kingsuk.panda.2004@gmail.com>
2024-05-13 11:48:17 +0530

    Made array to contain names upto 50 people

--- main.go
+++ main.go
@@ -6,19 +6,24 @@ import (
 
 func main() {
 	fmt.Printf("Welcome to the Test_repository!!")
+	var arr [50]string
+	fmt.Printf("\n\nCurrent array hosts nothing. Add a few names to your array! It can hold upto %d names!\n\nEnter the number of people you wanna add the names of: ", cap(arr))
 	var n int
-	fmt.Printf("\n\nEnter the size of the pattern (for example, 3 for 3x3): ")
 	fmt.Scanf("%d", &n)
-	arr := make([][]int, n)
 	for i := 0; i < n; i++ {
-		arr[i] = make([]int, n)
-	}
-	fmt.Printf("\n")
-	for i := 0; i < n; i++ {
-		for j := n; j > i; j-- {
-			fmt.Printf("*")
+		fmt.Printf("\nName of #%d person: ", i+1)
+		fmt.Scanf("%s", &arr[i])
+		if arr[i] == "#stop" {
+			break
+		} else if arr[i] == "" {
+			fmt.Println("Please enter a valid name.")
+			i--
 		}
-		fmt.Printf("\n")
 	}
-	fmt.Printf("\n")
+	fmt.Printf("\n\nThe names of the participants are as follows:\n")
+	for i := 0; i < n; i++ {
+		if arr[i] != "#stop" {
+			fmt.Printf("%d. %s\n", i+1, arr[i])
+		}
+	}
 }
