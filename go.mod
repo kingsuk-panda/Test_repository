73e41a4badaaef2cbfa4ea31dd6f0a9c9a96e931
kingsuk-panda <kingsuk.panda.2004@gmail.com>
2024-05-15 07:32:21 +0530

    Here's an implementation of the hashing using bcrypt in golang. I will use this to hass password in my student-mentorship-program project

---
+++ go.mod
@@ -0,0 +1,5 @@
+module Test_repository
+
+go 1.21.9
+
+require golang.org/x/crypto v0.23.0 // indirect
--- main.go
+++ main.go
@@ -2,28 +2,23 @@ package main
 
 import (
 	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 func main() {
-	fmt.Printf("Welcome to the Test_repository!!")
-	var arr [100]string
-	fmt.Printf("\n\nCurrent array hosts nothing. Add a few names to your array! It can hold upto %d names!\n\n***Say #stop to stop the input***", cap(arr))
-	n := cap(arr)
-	for i := 0; i < n; i++ {
-		fmt.Printf("\nName of #%d person: ", i+1)
-		fmt.Scanf("%s", &arr[i])
-		if arr[i] == "#stop" {
-			break
-		} else if arr[i] == "" {
-			fmt.Println("Please enter a valid name.")
-			i--
-		}
+	password := "mySecretPassword"
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		fmt.Println("Error generating hash:", err)
+		return
 	}
-	fmt.Printf("\n\nThe names of the participants are as follows:\n")
-	for i := 0; i < n; i++ {
-		if arr[i] == "#stop" {
-			break
-		}
-		fmt.Printf("%d. %s\n", i+1, arr[i])
+	fmt.Println("Hashed password:", string(hashedPassword))
+	newPassword := "mySecretPassword"
+	err = bcrypt.CompareHashAndPassword(hashedPassword, []byte(newPassword))
+	if err != nil {
+		fmt.Println("Password does not match hashed password.")
+		return
 	}
+	fmt.Println("Password matches hashed password.")
 }
